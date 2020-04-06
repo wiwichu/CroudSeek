048c214d6ee9ef4b6e30234cdb8ee0f7d58c16d1
wiwichu <mckeonppatrick@hotmail.com>
2020-04-06 15:49:25 -0400

    Users controller

---
+++ CroudSeek.API/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CroudSeek.API.Models;
+using CroudSeek.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CroudSeek.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly ICroudSeekRepository _croudSeekRepository;
+        private IMapper _mapper;
+
+        public UsersController(ICroudSeekRepository croudSeekRepository, IMapper mapper)
+        {
+            _croudSeekRepository = croudSeekRepository;
+            _mapper = mapper;
+        }
+        [HttpGet()]
+        public ActionResult<IEnumerable<UserDto>> GetUsers()
+        {
+            var usersFromRepo = _croudSeekRepository.GetUsers();
+            return Ok(_mapper.Map<IEnumerable<UserDto>>(usersFromRepo));
+        }
+
+        [HttpGet("{userId}", Name = "GetUser")]
+        public IActionResult GetUser(int userId)
+        {
+            var userFromRepo = _croudSeekRepository.GetUser(userId);
+
+            if (userFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<UserDto>(userFromRepo));
+        }
+
+    }
+}
---
+++ CroudSeek.API/Profiles/UsersProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CroudSeek.API.Profiles
+{
+    public class UsersProfile : Profile
+    {
+        public UsersProfile()
+        {
+            CreateMap<Entities.User, Models.UserDto>();
+        }
+    }
+}

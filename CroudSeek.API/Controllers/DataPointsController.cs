d2dcf5fe507d2b2dbaa19646dd9f192139d4fbc3
wiwichu <mckeonppatrick@hotmail.com>
2020-04-06 15:12:20 -0400

    DataPointController

---
+++ CroudSeek.API/Controllers/DataPointsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CroudSeek.API.Models;
+using CroudSeek.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CroudSeek.API.Controllers
+{
+    [ApiController]
+    [Route("api/datapoints")]
+
+    public class DataPointsController : ControllerBase
+    {
+        private readonly ICroudSeekRepository _croudSeekRepository;
+        private IMapper _mapper;
+
+        public DataPointsController(ICroudSeekRepository croudSeekRepository, IMapper mapper)
+        {
+            _croudSeekRepository = croudSeekRepository;
+            _mapper = mapper;
+        }
+        [HttpGet()]
+        public ActionResult<IEnumerable<DataPointDto>> GetDataPoints()
+        {
+            var dataPointsFromRepo = _croudSeekRepository.GetDataPoints();
+            return Ok(_mapper.Map<IEnumerable<DataPointDto>>(dataPointsFromRepo));
+        }
+
+        [HttpGet("{dataPointId}", Name = "GetDataPoint")]
+        public IActionResult GetQuest(int dataPointId)
+        {
+            var dataPointFromRepo = _croudSeekRepository.GetDataPoint(dataPointId);
+
+            if (dataPointFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<DataPointDto>(dataPointFromRepo));
+        }
+
+    }
+}
---
+++ CroudSeek.API/Profiles/DataPointsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CroudSeek.API.Profiles
+{
+    public class DataPointsProfile : Profile
+    {
+        public DataPointsProfile()
+        {
+            CreateMap<Entities.DataPoint, Models.DataPointDto>();
+        }
+    }
+}
